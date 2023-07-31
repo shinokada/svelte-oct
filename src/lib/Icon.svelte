1e98101e82552639e32708b85c0ed2d23470237b
Shinichi Okada <okada.shin@gmail.com>
2023-07-31 18:13:06 +0200

    fix: component doc update

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -35,7 +35,7 @@ viewBox="0 0 {displayIcon.box} {displayIcon.box}"
 
 <!--
 @component
-[Go to Document](https://shinokada.github.io/svelte-oct/)
+[Go to Document](https://svelte-oct.vercel.app/)
 ## Props
 @prop name;
 @prop width = "24";
